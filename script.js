63aec62e42499af9a50c0e8732298db0924f07a9
Tejas-Angadi-9 <tejasangadi456@gmail.com>
2023-07-04 19:23:37 +0530

    done

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
+// for sidemenu
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+  sidemenu.style.right = "0";
+}
+
+function closemenu(){
+  sidemenu.style.right = "-200px";
+}
+
 function openTab(currentTab) {
   for (tablink of tablinks) {
     tablink.classList.remove("active-link");
